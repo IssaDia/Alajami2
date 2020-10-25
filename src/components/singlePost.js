cba108b8c13f222442cb6c7d7fb114670ee6e9c0
IssaDia <issadiapro@gmail.com>
2020-10-25 21:25:26 +0100

    Next and previous ok

--- src/components/singlePost.js
+++ src/components/singlePost.js
@@ -1,12 +1,46 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Box from './box'
 import marked from 'marked'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-export default function SinglePost ({ title, content }) {
+export default function SinglePost ({ title, content, pageContext, categorySlug }) {
+  const prev = pageContext.prev ? {
+    url: `/themes/${categorySlug}/${pageContext.prev.slug}`,
+    title: pageContext.prev.title
+  } : null
+  const next = pageContext.next ? {
+    url: `/themes/${categorySlug}/${pageContext.next.slug}`,
+    title: pageContext.next.title
+  } : null
+  console.log(pageContext)
   return (
     <Box title={title}>
       <div>
         <div className='p-16 space-y-6 text-justify tracking-wider text-gray-800 text-lg font-playfair' dangerouslySetInnerHTML={{ __html: marked(content) }} />
+      </div>
+      <div className='text-xs lg:w-full px-8'>
+        <div className='float-left arrow-link-container my-8'>
+          {prev && (
+            <>
+              <Link to={prev.url} className='flex flex-wrap space-x-2'>
+                <FontAwesomeIcon icon={faArrowLeft} />
+                <span className='arrow-link-text'>{prev.title}</span>
+              </Link>
+            </>
+          )}
+        </div>
+        <div className='float-right arrow-link-container my-8'>
+          {next && (
+            <>
+              <Link to={next.url} className='flex flex-wrap space-x-2'>
+                <span className='arrow-link-text'>{next.title}</span>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </Box>
   )
--- src/templates/single-post-template.js
+++ src/templates/single-post-template.js
@@ -3,46 +3,13 @@ import React from 'react'
 import Layout from '../components/layout'
 import SinglePost from '../components/singlePost'
 import LastsPostsWidget from '../components/lastsPostsWidget'
-import { graphql, Link } from 'gatsby'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { graphql } from 'gatsby'
 
 export default function SinglePostTemplate ({ data, pageContext }) {
-  const prev = pageContext.prev ? {
-    url: `/themes/${data.contentfulBlogPost.postCategory.slug}/${pageContext.prev.slug}`,
-    title: pageContext.prev.title
-  } : null
-  const next = pageContext.next ? {
-    url: `/themes/${data.contentfulBlogPost.postCategory.slug}/${pageContext.next.slug}`,
-    title: pageContext.next.title
-  } : null
-  console.log(pageContext)
   return (
     <Layout>
       <div className='w-full lg:w-3/4 lg:px-4 sm:h-42 lg:m-0  mt-8'>
-        <SinglePost title={data.contentfulBlogPost.title} content={data.contentfulBlogPost.body.childMarkdownRemark.html} />
-        <div className='text-xs'>
-          <div className='float-left arrow-link-container my-8'>
-            {prev && (
-              <>
-                <Link to={prev.url} className='flex flex-wrap space-x-2'>
-                  <FontAwesomeIcon icon={faArrowLeft} />
-                  <span className='arrow-link-text'>{prev.title}</span>
-                </Link>
-              </>
-            )}
-          </div>
-          <div className='float-right arrow-link-container my-8'>
-            {next && (
-              <>
-                <Link to={next.url} className='flex flex-wrap space-x-2'>
-                  <span className='arrow-link-text'>{next.title}</span>
-                  <FontAwesomeIcon icon={faArrowRight} />
-                </Link>
-              </>
-            )}
-          </div>
-        </div>
+        <SinglePost title={data.contentfulBlogPost.title} content={data.contentfulBlogPost.body.childMarkdownRemark.html} pageContext={pageContext} categorySlug={data.contentfulBlogPost.postCategory.slug} />
       </div>
       <div className='w-full lg:w-1/4 px-4'>
         <LastsPostsWidget />
